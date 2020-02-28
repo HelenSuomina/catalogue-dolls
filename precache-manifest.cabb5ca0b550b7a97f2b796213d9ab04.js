72d7a916529f8afd20d6ea142b86c24f10f81e75
unknown <C:\Users\EDWARD\AppData\Roaming\The Bat!\Mail>
2020-02-28 17:00:56 +0300

    Updates

---
+++ precache-manifest.cabb5ca0b550b7a97f2b796213d9ab04.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca1ad4dfb554c9bef21561da08e93827",
+    "url": "/catalogue-dolls/index.html"
+  },
+  {
+    "revision": "762e6a0dfbb8a8295699",
+    "url": "/catalogue-dolls/static/css/2.e6a86b1b.chunk.css"
+  },
+  {
+    "revision": "657a274b09abf85bfa7c",
+    "url": "/catalogue-dolls/static/css/main.c610ab48.chunk.css"
+  },
+  {
+    "revision": "762e6a0dfbb8a8295699",
+    "url": "/catalogue-dolls/static/js/2.1cc8eb93.chunk.js"
+  },
+  {
+    "revision": "3b173ed738774b4bb64ac2b5ddb03f3b",
+    "url": "/catalogue-dolls/static/js/2.1cc8eb93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "657a274b09abf85bfa7c",
+    "url": "/catalogue-dolls/static/js/main.045c96b3.chunk.js"
+  },
+  {
+    "revision": "004ac191deb9aad77b07",
+    "url": "/catalogue-dolls/static/js/runtime-main.acdde4a9.js"
+  },
+  {
+    "revision": "60a9cb7a2a09ab9b3d1ea5f5fd8b563b",
+    "url": "/catalogue-dolls/static/media/bear_1.60a9cb7a.jpg"
+  },
+  {
+    "revision": "80b0aa4616f3a72a639efa9a2adb924a",
+    "url": "/catalogue-dolls/static/media/bear_2.80b0aa46.jpg"
+  },
+  {
+    "revision": "fbcb01412dcbdfdd1ef7ce942e3b7bfc",
+    "url": "/catalogue-dolls/static/media/bear_3.fbcb0141.jpg"
+  },
+  {
+    "revision": "883dacb9c386f2fad655abd9f0623b93",
+    "url": "/catalogue-dolls/static/media/brooch_1.883dacb9.jpg"
+  },
+  {
+    "revision": "11699397f53b30b7f7edb783a5239bf5",
+    "url": "/catalogue-dolls/static/media/brooch_2.11699397.jpg"
+  },
+  {
+    "revision": "15fd0b4a9110b71fbd924f80405c1e0d",
+    "url": "/catalogue-dolls/static/media/brooch_3.15fd0b4a.jpg"
+  },
+  {
+    "revision": "016a1defe52331346b560b00253c1f4d",
+    "url": "/catalogue-dolls/static/media/bunny_1.016a1def.jpg"
+  },
+  {
+    "revision": "8aaa36540d791f8ce0a1fd8a152f75c6",
+    "url": "/catalogue-dolls/static/media/bunny_2.8aaa3654.jpg"
+  },
+  {
+    "revision": "51015d70313afc3a1465a542b1803316",
+    "url": "/catalogue-dolls/static/media/bunny_3.51015d70.jpg"
+  },
+  {
+    "revision": "ca0e218cb7fe7a753daaf5c6c66bbfe0",
+    "url": "/catalogue-dolls/static/media/bunny_4.ca0e218c.jpg"
+  },
+  {
+    "revision": "bedb9941e0a1b0a84e4a2c8f8eaca47e",
+    "url": "/catalogue-dolls/static/media/fairy_1.bedb9941.jpg"
+  },
+  {
+    "revision": "0b2cde72af67133bcda9f2dee752fda6",
+    "url": "/catalogue-dolls/static/media/fairy_2.0b2cde72.jpg"
+  },
+  {
+    "revision": "82479ea1ddae9a055a164f52c1c36cf5",
+    "url": "/catalogue-dolls/static/media/fairy_3.82479ea1.jpg"
+  },
+  {
+    "revision": "4891bcf0d3a387af91885f2d8c1ebef4",
+    "url": "/catalogue-dolls/static/media/fairy_4.4891bcf0.jpg"
+  },
+  {
+    "revision": "b48d8ad146e2c64c2245f8b209910568",
+    "url": "/catalogue-dolls/static/media/fairy_5.b48d8ad1.jpg"
+  },
+  {
+    "revision": "855f8ec14f1bf77b53d51a2e685e1f18",
+    "url": "/catalogue-dolls/static/media/heart-fill.855f8ec1.svg"
+  },
+  {
+    "revision": "a2f1323e0cd698f93c92f2605f9fc964",
+    "url": "/catalogue-dolls/static/media/heart.a2f1323e.svg"
+  },
+  {
+    "revision": "b65b2a43813022c32c59a33a8675774c",
+    "url": "/catalogue-dolls/static/media/ls_logo.b65b2a43.png"
+  },
+  {
+    "revision": "222c2d653582535b5a5f9d9fe5eafd9f",
+    "url": "/catalogue-dolls/static/media/scary_1.222c2d65.jpg"
+  },
+  {
+    "revision": "9d2a4e8863ef501e8f77ceb1d9f3a5f6",
+    "url": "/catalogue-dolls/static/media/scary_2.9d2a4e88.jpg"
+  },
+  {
+    "revision": "8a00251dfab8b04bc439a3d43f2de899",
+    "url": "/catalogue-dolls/static/media/scary_3.8a00251d.jpg"
+  },
+  {
+    "revision": "b0526fb0fb529ab2156bcf5ce955d37f",
+    "url": "/catalogue-dolls/static/media/scary_4.b0526fb0.jpg"
+  },
+  {
+    "revision": "7e90e33f4202c200982818dd7c25cabb",
+    "url": "/catalogue-dolls/static/media/set_Bunny_Friends.7e90e33f.jpg"
+  },
+  {
+    "revision": "8c47056e8cf46d4db81ad7032946f9e3",
+    "url": "/catalogue-dolls/static/media/set_Donas.8c47056e.jpg"
+  },
+  {
+    "revision": "df8a74e89080659d02bee4443d322704",
+    "url": "/catalogue-dolls/static/media/set_Fairies.df8a74e8.jpg"
+  },
+  {
+    "revision": "68632446a257d9a404ced2d8f7b581f8",
+    "url": "/catalogue-dolls/static/media/set_Minnows.68632446.jpg"
+  },
+  {
+    "revision": "f60acf8f493896168e5b7f89f60b0097",
+    "url": "/catalogue-dolls/static/media/tilda_1.f60acf8f.jpg"
+  },
+  {
+    "revision": "a94153263cf4d9e2944c64b7b8e61476",
+    "url": "/catalogue-dolls/static/media/tilda_2.a9415326.jpg"
+  },
+  {
+    "revision": "edeb1f6856dd0fa2ddf6c33bc451f4e5",
+    "url": "/catalogue-dolls/static/media/tilda_3.edeb1f68.jpg"
+  },
+  {
+    "revision": "05f27919c48f36ac6e1136b91c07e296",
+    "url": "/catalogue-dolls/static/media/zoo_1.05f27919.jpg"
+  },
+  {
+    "revision": "a10d48a9a0b8ff97f7b30e4f6b21aee9",
+    "url": "/catalogue-dolls/static/media/zoo_2.a10d48a9.jpg"
+  },
+  {
+    "revision": "425bd251cbf803fc2b59782d38fc258e",
+    "url": "/catalogue-dolls/static/media/zoo_3.425bd251.jpg"
+  }
+]);
